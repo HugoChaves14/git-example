e213366ec19055b5162e9bf8ed7f907e91237bb3
hugo <hhchj14@gmail.com>
2020-11-24 22:22:01 -0500

    taller tercer corte

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 app = Flask(__name__)
+db = sqlite3.connect('data.db', check_same_thread=False)
+
 #rutas
-@app.route('/') #significa la ruta raiz
+@app.route('/', methods=['GET']) #significa la ruta raiz
 def index():
     return render_template('index.html')
 
@@ -27,4 +30,85 @@ def contacto():
 def sumar():
     suma=2+2
     return 'la suma de 2 + 2 es = '+str(suma)
+
+@app.route('/usuarios')
+def usuarios():
+    usuarios = db.execute('select * from usuarios')
+
+    usuarios = usuarios.fetchall()
+
+    return render_template('usuarios/listar.html', usuarios=usuarios)
+
+@app.route('/usuarios/crear', methods=['GET', 'POST'])
+def crear_usuarios():
+    if request.method == 'GET':
+        return render_template('usuarios/crear.html')
+
+    nombres = request.form.get('nombres')
+    apellidos = request.form.get('apellidos')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    cursor = db.cursor()
+    cursor.execute("""insert into usuarios(
+            nombres,
+            apellidos,
+            email,
+            password
+        )values (?,?,?,?)
+    """, (nombres, apellidos, email, password))
+
+    db.commit()
+
+    return redirect(url_for('usuarios'))
+
+
+@app.route('/usuarios/eliminar', methods=['GET','POST'])
+def eliminar_usuarios():
+    if request.method == 'GET':
+        return render_template('usuarios/eliminar.html')
+    id = request.form.get('id')
+    cursor = db.cursor()
+    cursor.execute('DELETE FROM usuarios WHERE id = ?',(id))
+    db.commit()
+    return redirect(url_for('usuarios'))
+
+@app.route('/usuarios/editar', methods=['GET','POST'])
+def editar_usuarios():
+    if request.method == 'GET':
+        return render_template('usuarios/editar.html')
+    id = request.form.get('id')
+    name = request.form.get('name')
+    lastname = request.form.get('lastname')
+    email = request.form.get('email')
+    passw = request.form.get('password')
+    cursor = db.cursor()
+    cursor.execute("""
+    UPDATE usuarios SET 
+    nombres = ?, 
+    apellidos = ?, 
+    email = ?, 
+    password = ? 
+    WHERE id = ?
+     """,(name,lastname,email,passw,id))
+    db.commit()
+    return redirect(url_for('usuarios'))
+
+@app.route('/usuarios/editar/<int:id>', methods=['GET','POST'])
+def boton_editar(id):
+    if request.method == 'GET':
+
+        return render_template('usuarios/editar.html')
+
+@app.route('/usuarios/eliminar/<int:id>', methods=['GET','POST'])
+def boton_borrar(id):
+    if request.method == 'GET':
+
+        return render_template('usuarios/eliminar.html')
+
 app.run(debug=True, port=5100)
+
+
+
+
+
